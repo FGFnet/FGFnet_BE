dbc0fcf3541aecb997250cd4347d7279c900dad6
김일건 <gun@gun-mba.local>
2024-10-13 23:14:25 +0900

    init: initial commit

---
+++ src/main/java/com/fg/fnet/FnetApplication.java
@@ -0,0 +1,13 @@
+package com.fg.fnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fg/fnet/FnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fg.fnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
