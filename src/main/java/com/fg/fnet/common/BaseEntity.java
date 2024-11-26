e7daae215d34cfdf26a4896425218b0f971cd1e9
gun <idmatthew9546@gmail.com>
2024-11-26 22:31:42 +0900

    feat(global): tsid setup

--- src/main/java/com/fg/fnet/common/BaseEntity.java
+++ src/main/java/com/fg/fnet/common/BaseEntity.java
@@ -1,9 +1,8 @@
 package com.fg.fnet.common;
 
+import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -20,7 +19,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseEntity {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Tsid
   private Long id;
 
   @CreatedDate
